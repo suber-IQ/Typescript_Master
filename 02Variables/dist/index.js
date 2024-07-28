d2b61fa14fb153b133b5e44e9a6f1c54f42aea35
sumit <suberquercusumit@gmail.com>
2024-07-28 14:27:23 +0530

    add variable section

---
+++ 02Variables/dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var userName = "Sumit";
+var num1 = 3;
+var num2 = 5;
+var num2 = 9; // after asign same value into value first is delete(Override) then second assign 9 
+var addtion = num1 + num2; // output: 12
+var multiple = num1 * num2; // output: 27
+var division = num2 / num1; // output:  3
+console.log(userName);
+console.log(addtion);
+console.log(multiple);
+console.log(division);
+//👉 Variable names
+var $a = "String";
+var _b = 23.5;
+console.log($a);
+console.log(_b);
---
+++ 02Variables/src/index.ts
@@ -0,0 +1,35 @@
+var userName: string = "Sumit";
+var num1: number = 3;
+var num2: number = 5;
+var num2: number = 9; // after asign same value into value first is delete(Override) then second assign 9 
+var addtion: number = num1 + num2;  // output: 12
+var multiple: number = num1 * num2;  // output: 27
+var division: number = num2 / num1;  // output:  3
+console.log(userName);
+console.log(addtion);
+console.log(multiple);
+console.log(division);
+
+//👉 Variable names
+
+var $a: string = "String";
+var _b: number = 23.5;
+var _234b: number = 23.5;
+// var 5d: boolean = false;  // 🎄 Numbers are not allowed as the first character of variable
+console.log($a);
+console.log(_b);
+console.log(_234b);
+
+
+
+var studentName: string = "Amit" // 🎄 camelCase (commanly used)
+var user;
+var num;
+var userNum;
+var MyName: string = "Sumit" // PascalCase
+
+
+// 👉 comment (// (single line) or /* */ (multiple lines))
+
+// var studentName = "Student" 
+/* var empName; */
