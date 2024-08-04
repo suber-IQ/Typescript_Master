89359a266fdce3e632b2989810f7717d67c0a998
sumit <suberquercusumit@gmail.com>
2024-08-04 23:59:16 +0530

    add Arrays

---
+++ 07Arrays/dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+// var rollNo:number = 23;
+// var rollNo:number = 5;
+// var rollNo:number = 3;
+// var rollNo:number = 2;
+// 👉 section 1 (Array) ( always starting with zero index)
+// var studentRollNo:number[] = [10,20,30,50,100,400];
+// console.log(studentRollNo);
+// console.log(studentRollNo[2]);
+// console.log(studentRollNo[0]);
+// console.log(studentRollNo[1]);
+// console.log(studentRollNo[3]);
+// console.log(studentRollNo[4]);
+// console.log(studentRollNo[5]);
+// console.log(studentRollNo[6]); //output: undefined
+// 👉 section 2 (Update)
+// var studentRollNo:number[] = [10,20,30,50,100,400];
+// studentRollNo[2] = 88;
+// console.log(studentRollNo[2]); //output: 88
+// console.log(studentRollNo); // output:  [10,20,88,50,100,400];
+// 👉 section 3 
+let studentName = ["Sumit", "Amit", "Kajal", "Priyanka", "Ajali"];
+studentName[5] = "Aditaya";
+console.log(studentName);
