f25eb453422319466ae942d2c78ed6e57d4c14b0
sumit <suberquercusumit@gmail.com>
2024-08-08 22:47:44 +0530

    add for and while loops

---
+++ 08Loops/src/index.ts
@@ -0,0 +1,72 @@
+
+// section : 1 
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// section : 2
+
+// function printConsole(){
+//     console.log("Hello world"); // ten times hello world print
+// }
+
+
+// printConsole();
+// printConsole();
+// printConsole();
+// printConsole();
+// printConsole();
+// printConsole();
+// printConsole();
+// printConsole();
+// printConsole();
+// printConsole();
+
+
+// section : 3
+
+// console.log("start loop");
+
+// for(let i = 1; i <= 5;i++){
+//     // console.log(1);
+//     // console.log(2);
+//     console.log(i);    
+// }
+// console.log("end loop");
+
+
+// section : 4
+
+// while(false){
+//     console.log(index); // not print
+    
+// }
+
+// let index:number = 0;
+
+// while(true){
+// while(2 < 5){
+// while(index < 5){ // index: 0, index: 1,index: 2,index: 3,index: 4
+    // console.log(index);
+    // index = index + 1;
+// }
+
+// section 5: 
+// 👉 Write a ts program, that print the table of 2
+
+// let index: number = 1;
+
+// while(index <= 10){
+//     console.log(2 * index);
+//     index = index + 1;
+// }
+
+
+
