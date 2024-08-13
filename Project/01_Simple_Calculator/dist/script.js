1e1deb9e8e5a1981d52796c87dfbc87acde77ba4
sumit <suberquercusumit@gmail.com>
2024-08-13 23:44:13 +0530

    add simple calculator project

---
+++ Project/01_Simple_Calculator/dist/script.js
@@ -0,0 +1,31 @@
+"use strict";
+let output = document.getElementById("output");
+let buttons = document.getElementsByTagName("button");
+Array.from(buttons).forEach((button) => {
+    button.addEventListener("click", (e) => {
+        e.preventDefault(); // Prevent default button behavior
+        if (output) {
+            if (button.value === "AC") {
+                // Clear the output if "AC" button is clicked
+                output.value = "";
+            }
+            else if (button.value === "=") {
+                // Evaluate the expression if "=" button is clicked
+                try {
+                    // Use Function constructor to evaluate expression safely
+                    const result = new Function('return ' + output.value)();
+                    output.value = String(result.toFixed(2));
+                }
+                catch (error) {
+                    // Handle any errors that occur during evaluation
+                    console.error('Invalid expression:', error);
+                    output.value = "Error";
+                }
+            }
+            else {
+                // Append the button value to the output if it's a number or operator
+                output.value += button.value;
+            }
+        }
+    });
+});
